ce802ca94c85f28ec7e171f0e294ad40109de6c0
Kevin Wang <kevinw041231@gmail.com>
2026-07-09 00:09:44 -0400

    Set up docker env and verified build success

--- kortex_description/launch/view_robot.launch.py
+++ kortex_description/launch/view_robot.launch.py
@@ -106,6 +106,7 @@ def generate_launch_description():
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
         name="joint_state_publisher_gui",
+        parameters=[robot_description],
     )
 
     rviz = Node(
